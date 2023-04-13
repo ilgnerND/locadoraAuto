dc2d7ca591f69ffe2c125d149e6117515530953b
Ilgner Nunes de Deus <ilgnern@icloud.com>
2023-04-12 21:26:40 -0300

    Início da atividade prática 1, adição das classes Veiculo, Carro, Onibus e Locadora. Assim como seus respectivos atributos, métodos construtores e get sets.

---
+++ src/Locadora.java
@@ -0,0 +1,28 @@
+// Classe Locadora que gerencia os veículos
+class Locadora {
+    private Veiculo[] veiculos;
+
+    // Construtor
+    public Locadora() {
+        veiculos = new Veiculo[100]; // Tamanho máximo de 100 veículos, pode ser ajustado conforme necessário
+    }
+
+    // Métodos para adicionar e listar veículos
+    public void adicionarVeiculo(Veiculo veiculo) {
+        // Lógica para adicionar veículo na lista
+    }
+
+    public void listarVeiculos() {
+        // Lógica para listar os veículos disponíveis
+    }
+
+    // Métodos para locar e devolver veículos
+    public void locarVeiculo(String placa) {
+        // Lógica para locar um veículo pelo número da placa
+    }
+
+    public void devolverVeiculo(String placa) {
+        // Lógica para devolver um veículo pelo número da placa
+    }
+    
+}
